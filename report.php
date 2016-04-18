2b2fa27b3e80b737051037f564627e9765617a3e
alanpilloud <alanpilloud@gmail.com>
2016-04-18 22:22:52 +0200

    AUTHORIZED_UA is defined in config.php

--- report.php
+++ report.php
@@ -1,9 +1,6 @@
 <?phpinclude ('config.php');
-define('AUTHORIZED_UA', 'bwap-monitoring');
-/***  Just ensure that there is a "secured" call*/
-$_GET['key'] === KEY or die;$_SERVER['HTTP_USER_AGENT'] == AUTHORIZED_UA or die;
-/***  Now, let's go !*/
-$tool_info = array(    'version' => 1.1);
+/***  Just ensure that there is a "secured" call*/$_GET['key'] === KEY or die;$_SERVER['HTTP_USER_AGENT'] == AUTHORIZED_UA or die;
+/***  Now, let's go !*/$tool_info = array(    'version' => 1.1);
 include('../wp-load.php');
 if ( ! function_exists( 'get_plugins' ) ) {    require_once ABSPATH . 'wp-admin/includes/plugin.php';}
 $data = array(    'bwap-monitoring' => $tool_info,    'system' => array(        'version' => get_bloginfo('version')    ),    'themes' => wp_get_themes(),    'plugins' => get_plugins(),    'mu-plugins' => get_mu_plugins());
