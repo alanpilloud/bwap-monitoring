a0fd701bd9d4de034a2d319011402b4dd0a7cdcb
alanpilloud <alanpilloud@gmail.com>
2016-05-24 20:44:01 +0200

    1.2 -> got php and mysql versions

--- report.php
+++ report.php
@@ -1,7 +1,39 @@
-<?phpinclude ('config.php');
-/***  Just ensure that there is a "secured" call*/$_GET['key'] === KEY or die;$_SERVER['HTTP_USER_AGENT'] == AUTHORIZED_UA or die;
-/***  Now, let's go !*/$tool_info = array(    'version' => 1.1);
-include('../wp-load.php');
-if ( ! function_exists( 'get_plugins' ) ) {    require_once ABSPATH . 'wp-admin/includes/plugin.php';}
-$data = array(    'bwap-monitoring' => $tool_info,    'system' => array(        'version' => get_bloginfo('version')    ),    'themes' => wp_get_themes(),    'plugins' => get_plugins(),    'mu-plugins' => get_mu_plugins());
+<?php
+include ('config.php');
+
+/**
+*  Just ensure that there is a "secured" call
+*/
+
+$_GET['key'] === KEY or die;
+$_SERVER['HTTP_USER_AGENT'] == AUTHORIZED_UA or die;
+
+/**
+*  Now, let's go !
+*/
+
+$tool_info = array(
+    'version' => 1.2
+);
+
+include('../wp-load.php');
+
+if ( ! function_exists( 'get_plugins' ) ) {
+    require_once ABSPATH . 'wp-admin/includes/plugin.php';
+}
+
+global $wpbd;
+
+$data = array(
+    'bwap-monitoring' => $tool_info,
+    'system' => array(
+        'version' => get_bloginfo('version'),
+        'php-version' => phpversion(),
+        'mysql-version' => $wpdb->db_version()
+    ),
+    'themes' => wp_get_themes(),
+    'plugins' => get_plugins(),
+    'mu-plugins' => get_mu_plugins()
+);
+
 echo json_encode($data);
