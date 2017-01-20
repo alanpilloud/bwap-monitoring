39d7e0d6f5e7becf65c3b0c9c8c98a847476b1f4
alanpilloud <alanpilloud@gmail.com>
2017-01-20 11:30:57 +0100

    add configurable paths

--- sphinge/config.sample.php
+++ sphinge/config.sample.php
@@ -3,3 +3,17 @@
 *  Secret key for accessing these informations
 */
 define('KEY', '');
+
+/**
+*  WordPress wp-load.php Path
+*
+*  If your WordPress install is standard, then you don't need to adapt this path.
+*/
+define('WP_LOAD_PATH', '..');
+
+/**
+*  WordPress admin Path
+*
+*  If your WordPress install is standard, then you don't need to adapt this path.
+*/
+define('WP_ADMIN_PATH', 'wp-admin');
