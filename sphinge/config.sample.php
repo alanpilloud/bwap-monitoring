7be7d675ab809544ddb044a1c9cb67687041b958
alanpilloud <alanpilloud@gmail.com>
2017-02-01 22:43:20 +0100

    add SPHINGE_URL to config

--- sphinge/config.sample.php
+++ sphinge/config.sample.php
@@ -17,3 +17,10 @@ define('WP_LOAD_PATH', '..');
 *  If your WordPress install is standard, then you don't need to adapt this path.
 */
 define('WP_ADMIN_PATH', 'wp-admin');
+
+/**
+*  Sphinge URL
+*
+*  Some extensions might require you to provide your dashboard's url
+*/
+//define('SPHINGE_URL', '');
